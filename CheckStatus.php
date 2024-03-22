68f0f852b7d7557213765cc11c9d66f7f8e78469
dhanifinservices <163349412+dhanifinservices@users.noreply.github.com>
2024-03-22 05:26:13 -0700

    update

---
+++ CheckStatus.php
@@ -0,0 +1,183 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Apply Online For Rajasthan Kusam Yojana 2021 | Application Form Kusum Solar Pump Yojana | कुसुम योजना ऑनलाइन आवेदन 2021 | Online Registration Kusum Scheme" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="कुसुम योजना ऑनलाइन आवेदन 2021: Kusum Yojana Registration, आवेदन फार्म" />
+    <meta property="og:description" content="Apply Online For Rajasthan Kusam Yojana 2021 | Application Form Kusum Solar Pump Yojana | कुसुम योजना ऑनलाइन आवेदन 2021 | Online Registration Kusum Scheme" />
+    <meta property="og:url" content="https://farmersolutionsolaryojna.in/" />
+    <meta property="article:tag" content="Apply Online Kusam Solar Pump Yojana" />
+    <meta property="article:tag" content="Rajasthan Kusum Yojana Application Form" />
+    <meta property="article:tag" content="कुसुम योजना ऑनलाइन आवेदन" />
+    <meta property="article:tag" content="कुसुम योजना ऑनलाइन एप्लीकेशन फॉर्म" />
+    <meta property="article:tag" content="कुसुम योजना राजस्थान" />
+    <meta property="article:section" content="Rajasthan Govt Scheme" />
+    <meta property="article:published_time" content="2021-01-21T05:30:00+00:00" />
+    <meta property="article:modified_time" content="2021-01-21T07:10:35+00:00" />
+    <meta property="og:updated_time" content="2021-01-21T07:10:35+00:00" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:description" content="Apply Online For Rajasthan Kusam Yojana 2021 | Application Form Kusum Solar Pump Yojana | कुसुम योजना ऑनलाइन आवेदन 2021 | Online Registration Kusum Scheme" />
+    <meta name="twitter:title" content="कुसुम योजना ऑनलाइन आवेदन 2021: Kusum Yojana Registration, आवेदन फार्म" />
+    <title>Online Kusum Yojona</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/Content/Img/favicon.png">
+    <link href="../../../css.css?family=Raleway:300,400,700,800%7CLato:300,400,700" rel="stylesheet" type="text/css">
+    <link href="./css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="./css/font-awesome.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="./css/ionicons.css" rel="stylesheet" type="text/css">
+    <link href="./css/simple-line-icons.css" rel="stylesheet" type="text/css">
+    <link href="./css/flaticon.css" rel="stylesheet" type="text/css">
+    <!--Main Slider-->
+    <link href="./css/settings.css" type="text/css" rel="stylesheet" media="screen">
+    <link href="./css/layers.css" type="text/css" rel="stylesheet" media="screen">
+    <!--Light box-->
+    <link href="./css/jquery.fancybox.css" rel="stylesheet" type="text/css">
+    <!-- carousel -->
+    <link href="./css/owl.carousel.css" rel="stylesheet" type="text/css">
+    <link href="./css/owl.transitions.css" rel="stylesheet" type="text/css">
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="./css/index.css" rel="stylesheet">
+    <link href="./css/header.css" rel="stylesheet">
+    <link href="./css/footer.css" rel="stylesheet">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.js"></script>
+
+    <link href="/css/sweetalert.css" rel="stylesheet" />
+    <script src="/js/sweetalert.js"></script>
+    <script src="/js/sweetalert.min.js"></script>
+
+
+</head>
+<body>
+    	<?php
+        if(isset($_SESSION['error'])){
+          echo "
+           <script type='text/javascript'>
+		   new Noty({
+			    theme: 'sunset',
+				type: 'error',
+				layout: 'topRight',
+				text: '".$_SESSION['error']."',
+				timeout: 3000
+			}).show();
+		   </script>
+          ";
+          unset($_SESSION['error']);
+        }
+        if(isset($_SESSION['success'])){
+          echo "
+            <script type='text/javascript'>
+		   new Noty({
+			    theme: 'sunset',
+				type: 'success',
+				layout: 'topRight',
+				text: '".$_SESSION['success']."',
+				timeout: 3000
+			}).show();
+		   </script>
+          ";
+          unset($_SESSION['success']);
+        }
+      ?>
+    <!--loader-->
+    <!--loader-->
+    <!--Header Section Start Here
+            ==================================-->
+    
+    <!--Section End Here-->
+    <!--Start mainmenu area-->
+    <!-- CONTENT -->
+    <!-- Intro Section -->
+    
+    <!-- End Intro Section -->
+    <!-- Login Section -->
+    <div id="login" class="padding ptb-xs-60 page-signin">
+        <div class="container">
+            <div class="row">
+                <div class="main-body">
+                    <div class="body-inner">
+                        <div class="card bg-white">
+                            <div class="card-content">
+                                <section class="logo text-center">
+                                    <h2>Check Status</h2>
+                                </section>
+                                <form action="YourApplicationStaus.php" method="POST" class="form-horizontal ng-pristine ng-valid">
+                                    <fieldset>
+                                        <div class="form-group">
+                                            <div class="ui-input-group">
+                                                <input type="text" name="phone" required="" class="form-control" id="txtchkphn" maxlength="10" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" required>
+                                                <span class="input-bar"></span>
+                                                <label>Your Contact Number</label>
+                                            </div>
+                                        </div>
+                                        <div class="spacer"></div>
+                                    </fieldset>
+                               
+                            </div>
+                            <div class="card-action no-border text-right">
+                                <button type="submit">Check</button>
+                            </div>
+                             </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Login Section -->
+    <!--End Contact-->
+    <!-- FOOTER -->
+    
+    <!-- END FOOTER -->
+
+
+   <script type="text/javascript" src="./js/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/tether.min.js"></script>
+    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+    <!-- revolution Js -->
+    <script type="text/javascript" src="./js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="./js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="./js/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="./js/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="./js/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="./js/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="./js/jquery.revolution.js"></script>
+    <!-- fancybox Js -->
+    <script src="./js/jquery.mousewheel-3.0.6.pack.js" type="text/javascript"></script>
+    <script src="./js/jquery.fancybox.pack.js" type="text/javascript"></script>
+    <!-- masonry,isotope Effect Js -->
+    <script src="./js/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+    <script src="./js/isotope.pkgd.min.js" type="text/javascript"></script>
+    <script src="./js/masonry.pkgd.min.js" type="text/javascript"></script>
+    <script src="./js/jquery.appear.js" type="text/javascript"></script>
+    <!-- parallax Js -->
+    <script src="./js/jquery.parallax-1.1.3.js" type="text/javascript"></script>
+    <script src="./js/jquery.appear.js" type="text/javascript"></script>
+    <!-- matchHeight Js -->
+    <script src="./js/jquery.matchHeight-min.js" type="text/javascript"></script>
+    <!-- carousel Js -->
+    <script src="./js/owl.carousel.js" type="text/javascript"></script>
+    <!-- Accordion Js -->
+    <script type="text/javascript" src="./js/jquery.accordion.js"></script>
+    <!-- map -->
+    <script type="text/javascript" src="../../../maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="./js/map.js"></script>
+    <script type="text/javascript" src="./js/validation.js"></script>
+    <script src=".js/custom.js" type="text/javascript"></script>
+    <script src="../../../hosting_popup/js/hots_popup.js"></script>
+	<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+	<script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+<script src="https://unpkg.com/ionicons@5.5.1/dist/ionicons.js"></script>
+
+</body>
+
+</html>
+
+
